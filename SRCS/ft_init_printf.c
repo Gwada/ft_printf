5986e8c1fa2c67972b44112ede3a3a5df0ef8730
Dimitri LAVAURY-COLLOT <dlavaury@e3r13p13.42.fr>
2017-12-16 20:27:11 +0100

    ++

--- SRCS/ft_init_printf.c
+++ SRCS/ft_init_printf.c
@@ -6,7 +6,7 @@
 /*   By: dlavaury <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 11:22:51 by dlavaury          #+#    #+#             */
-/*   Updated: 2017/12/16 19:24:40 by dlavaury         ###   ########.fr       */
+/*   Updated: 2017/12/16 20:26:39 by dlavaury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 void	ft_init_printf(t_data *data, const char *s, int fd)
 {
-	//printf("in init printf\n");
+	printf("->in init printf | ");//
 	ft_bzero(data, sizeof(*data));
 	data->ft = (char*)s;
 	data->fd = fd;
-	//printf("out init printf\n");
+	printf("out init printf<-\n");//
 }
